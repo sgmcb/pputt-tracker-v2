85343c15539bb434ad66af4dc0f413111b599eaa
Sean McB <in@sgmcb.com>
2018-09-30 11:52:37 -0700

    refactor
    Sorting coursePos and Score editing into their own sections

--- js/main.js
+++ js/main.js
@@ -15,45 +15,50 @@ var totalScore = 0; // Holds the total course score
 var editingHole;
 
 var estCoursePos = 1;   // Holds the position (i.e. hole) where we believe that the player is on the course
-
-
+var posWindowW = 2;  // How large of a window do we leave editable around the estimated position?
 
 
 
 // On click/tap
 $( ".hole-row.active" ).click(function() {
+  
+  // Which hole are we editing with this click?
   editingHole = $(this).attr("id");
   console.log("Editing hole " + editingHole);
   
+  // COURSE POSITION UPDATES
   
+  // estCoursePos is updated, but only ever increases
+  if ( editingHole > estCoursePos ) {
+    estCoursePos = parseInt(editingHole) + 1;
+    console.log("New estCoursePose="+estCoursePos);
+  }
+
+  // Updating estimated course position
+  if(editingHole >= (estCoursePos+posWindowW)) {
+    
+    
+  }
+  
+  // UPDATE THE SCORE  
   holeScore = $( this ).find(".hole-score").html();
-  
-  
-  
-  // What to do if the score is at 6...  
+
+  // IFF Score=6  
   if(holeScore==6) {
     console.log("Score cannot exceed 6.");
     holeScore=1;
     totalScore = totalScore - 5;
   }
   
-  // Regular score
+  // Score<6
   else {
     holeScore++;
     totalScore++;    
   }
-  
-  
-  
-  
-
-  
-  // Increment the total score here
-  //$( this ).find(".hole-score").html(holeScore);
-  $( this ).find(".h"+editingHole).html(holeScore);
-  
-  // Increment the total score
-  $(".ts-number").html(totalScore);
+   
+  // Update score fields
+  $( this ).find(".h"+editingHole).html(holeScore);   // Hole score
+  $(".ts-number").html(totalScore);                   // Total score
   
   
   console.log("score click" + holeScore);
